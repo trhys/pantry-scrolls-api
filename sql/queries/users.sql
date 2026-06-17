0f4e2b1703d87d9b6dc23f231031fec6abb0be04
Tim Reese <treese1993@gmail.com>
2026-06-17 15:11:29 +0000

    Update users.sql

--- sql/queries/users.sql
+++ sql/queries/users.sql
@@ -49,9 +49,13 @@ WHERE email = $1;
 
 -- name: UpdateUser :exec
 UPDATE users
-SET name = $2, hashed_pw = $3, updated_at = NOW()
+SET name = $2, updated_at = NOW()
 WHERE id = $1;
 
+-- name: UpdatePasswordHash :exec
+UPDATE users 
+SET hashed_pw = $2, updated_at = NOW()
+WHERE id = $1;
 -- name: VerifyEmail :exec
 UPDATE users
 SET is_verified = TRUE
