0dedbfa3f0ecb14ac13fa31d908b1196ac8b61cc
Tim Reese <treese1993@gmail.com>
2026-06-19 11:07:10 -0500

    added metrics middleware

--- internal/server/middleware.go
+++ internal/server/middleware.go
@@ -9,6 +9,10 @@ import (
 	"strings"
 
 	"github.com/golang-jwt/jwt/v5"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"github.com/trhys/Recipe-Repo-2/internal/metrics"
 	"github.com/trhys/Recipe-Repo-2/internal/auth"
 )
 
@@ -68,4 +72,23 @@ func getClientIP(r *http.Request) string {
 		return r.RemoteAddr
 	}
 	return host
+}
+
+func (cfg *ApiConfig) MetricsMiddleware(m *metrics.Metrics) func (http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			start := time.Now()
+			
+			path := r.Pattern
+			if path == "" {
+			    path = "unknown"
+			}
+	
+			next.ServeHTTP(w, r)
+	
+			duration := time.Since(start).Seconds()
+			m.Latency.WithLabelValues(path).Observe(duration)
+			m.ServerHits.WithLabelValues(path).Inc()
+		})
+	}
 }
