dae66c5036f1ea0508fee21b6e31800963b1381e
Tim Reese <treese1993@gmail.com>
2026-06-30 09:43:47 -0500

    Add GET endpoint for retrieving messages

--- internal/server/init.go
+++ internal/server/init.go
@@ -62,8 +62,9 @@ func GetRouter(cfg *ApiConfig, reg *prometheus.Registry) *http.ServeMux {
 	mux.HandleFunc("GET /api/tokens/refresh", cfg.handlerRefreshToken)
 	mux.HandleFunc("GET /api/tokens/revoke", cfg.handlerRevokeToken)
 
- // Messages eps
- mux.HandleFunc("POST /api/messages", cfg.handlerAddMessage)
+	// Messages eps
+	mux.HandleFunc("POST /api/messages", cfg.handlerAddMessage)
+	mux.HandleFunc("GET /api/messages", cfg.handlerGetMessages)
 
 	return mux
 }
