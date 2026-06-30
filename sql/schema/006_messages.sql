2f04316f84abaeb310c57e6cb38477c482f7449d
Tim Reese <treese1993@gmail.com>
2026-06-30 09:49:20 -0500

    Fix primary key definition in messages table

--- sql/schema/006_messages.sql
+++ sql/schema/006_messages.sql
@@ -1,11 +1,11 @@
 -- +goose Up
 CREATE TABLE messages (
+  id UUID PRIMARY KEY,
   user_email TEXT NOT NULL,
   message TEXT NOT NULL,
   resolved BOOLEAN NOT NULL DEFAULT FALSE,
   status TEXT NOT NULL DEFAULT 'unread',
-  FOREIGN KEY (user_email) REFERENCES users(email),
-  PRIMARY KEY (user_email, message)
+  FOREIGN KEY (user_email) REFERENCES users(email)
 );
 
 -- +goose Down
