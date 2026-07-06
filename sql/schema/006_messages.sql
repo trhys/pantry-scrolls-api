1816473b7b0c15d1d74079f425c1a20b69404d49
Tim Reese <treese1993@gmail.com>
2026-07-06 11:39:40 -0500

    Fix syntax error in messages table schema

--- sql/schema/006_messages.sql
+++ sql/schema/006_messages.sql
@@ -4,9 +4,8 @@ CREATE TABLE messages (
   user_email TEXT NOT NULL,
   message TEXT NOT NULL,
   resolved BOOLEAN NOT NULL DEFAULT FALSE,
-  status TEXT NOT NULL DEFAULT 'unread',
-  FOREIGN KEY (user_email) REFERENCES users(email)
-);
+  status TEXT NOT NULL DEFAULT 'unread'
+  );
 
 -- +goose Down
 DROP TABLE messages;
