2ef386389814e8c81004e25a0f2f85f8bf58869b
trhys <treese1993@gmail.com>
2026-07-15 19:17:46 -0500

    fix: db init broken - moving seed to manual exec

--- cmd/scripts/seed.sh
+++ cmd/scripts/seed.sh
@@ -1,10 +1,6 @@
 #!/bin/sh
 set -e
 
-# Start postgres using the official entrypoint in the background
-docker-entrypoint.sh postgres &
-PG_PID=$!
-
 # Wait for postgres to accept connections
 echo "Waiting for postgres to be ready..."
 until pg_isready -U "$POSTGRES_USER" -d "$POSTGRES_DB"; do
@@ -28,6 +24,3 @@ else
 fi
 
 echo "DB init complete."
-
-# Keep postgres running
-wait $PG_PID
