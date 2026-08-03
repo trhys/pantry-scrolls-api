6a01d48779399ea61385756e22aafd56a657f9db
Tim Reese <treese1993@gmail.com>
2026-08-03 11:59:40 -0500

    Add SQL queries for managing recipe tags

---
+++ sql/queries/recipeTags.sql
@@ -0,0 +1,10 @@
+-- name: AddRecipeTags :exec
+INSERT INTO recipe_tags (recipe_id, tag)
+VALUES (
+  $1,
+  SELECT * FROM UNNEST(@tags::text[]))
+);
+
+-- name: RemoveRecipeTag :exec
+DELETE FROM recipe_tags
+WHERE recipe_id = $1 AND tag = $2;
