e910eaaf7984ba329380d0f1af23a533ad94d2a4
Tim Reese <treese1993@gmail.com>
2026-08-06 14:09:10 -0500

    Add SQL queries for category management

--- sql/queries/ingredients.sql
+++ sql/queries/ingredients.sql
@@ -18,3 +18,12 @@ WHERE id = $1;
 -- name: GetIngredientFromName :one
 SELECT id FROM ingredients
 WHERE name = $1;
+
+-- name: AddCategory :exec
+UPDATE ingredients
+SET category = $1
+WHERE name = $2;
+
+-- name: GetCategory :many
+SELECT * FROM ingredients
+WHERE category = $1;
