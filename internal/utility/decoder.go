d67a15cc0bda1666b44d74ec0843f3917e4b2a88
Tim Reese <treese1993@gmail.com>
2026-08-07 12:43:51 -0500

    Remove CheckNilSlice function from decoder.go
    Removed CheckNilSlice function that validated non-empty slices in request bodies.

--- internal/utility/decoder.go
+++ internal/utility/decoder.go
@@ -17,15 +17,3 @@ func DecodeRequest(w http.ResponseWriter, r *http.Request, maxBytes int64, T any
 
 	return nil
 }
-
-// This helper is to check slices in our request bodies that should not be empty
-// such as ingredients
-func CheckNilSlice(s [][]any) error {
-	for _, i := range s {
-		if len(i) == 0 {
-			return fmt.Errorf("Empty slice in request body")
-		}
-	}
-
-	return nil
-}
